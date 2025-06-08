51fc0ae29520866f991837f67fde79f98fcae50f
Avro <memergamerstudio@gmail.com>
2025-06-07 20:05:20 -0400

    Slight change

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,4 +1,4 @@
-﻿ReadingAndWriting readingAndWriting = new ReadingAndWriting("lelelet");
+﻿ReadingAndWriting readingAndWriting = new ReadingAndWriting("FILE'S NAME HERE!!");
 readingAndWriting.ReadFile();
 
 System.Console.WriteLine(readingAndWriting.FileContents);
